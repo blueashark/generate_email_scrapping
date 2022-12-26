7b700c751d134e9f730263f28426635ccc0c77a0
devrouis <80670036+devrouis@users.noreply.github.com>
2022-12-26 07:32:33 -0500

    initial project

--- email_finder.py
+++ email_finder.py
@@ -1,19 +1,18 @@
-import requests
-import time
-
-BIG_API = "Enter your Bigpicture API"
+from bs4 import BeautifulSoup
+import cloudscraper
 
 
 class EmailFinder:
 
     def __init__(self):
-        self.author = []
-        self.inst = []
-        self.email_id = []
+        self.author = str()
+        self.inst_link = str()
+        self.email_id = str()
+        self.website = str()
 
-    def get_email(self, author, inst):
+    def get_email(self, author, inst_link):
         self.author = author
-        self.inst = inst
+        self.inst_link = inst_link
 
         # Prepare fname_lname
         fname = str()
@@ -24,21 +23,29 @@ class EmailFinder:
         lname = str(split_name[-1])
         fname_lname = fname + "." + lname
 
-        # API call to convert from name to domain
-        if self.inst == " ":
-            domain = "NotFound"
+        # Beautifulsoup to convert from name to domain
+        if self.inst_link != " ":
+            scraper = cloudscraper.create_scraper(delay=10, browser='chrome')
+            url = self.inst_link
+            info = scraper.get(url).text
+            soup = BeautifulSoup(info, "html.parser")
+
+            div_text_list = []
+            for div in soup.findAll('div', attrs={'class': 'nova-legacy-e-text nova-legacy-e-text--size-m nova-legacy-e-text--family-sans-serif nova-legacy-e-text--spacing-none nova-legacy-e-text--color-inherit'}):
+                div_text_list.append(div.text)
+            if "Website" in div_text_list:
+                domain_index = div_text_list.index("Website") + 1
+                self.website = div_text_list[domain_index]
+                if div_text_list[domain_index][0] == "h":
+                    domain = div_text_list[domain_index].split("/")[2].replace("www.", "@")
+                elif div_text_list[domain_index][0] == "w":
+                    domain = div_text_list[domain_index].replace("www.", "@")
+            else:
+                domain = "@NotFound"
+                self.website = "NotFound"
         else:
-            bp_url_nd = "https://company.bigpicture.io/v2/companies/search"
-            params_nd = {"name": self.inst}
-            headers_nd = {
-                    "Authorization": BIG_API
-                }
-            response_nd = requests.request("GET", bp_url_nd, headers=headers_nd, params=params_nd).json()
-            try:
-                domain = response_nd["data"][0]["domain"]
-            except IndexError and KeyError:
-                domain = "NotFound"
-            time.sleep(0.5)
+            domain = "@NotFound"
+            self.website = "NotFound"
 
         # Merge domain list and fname_lname list
-        self.email_id = str(fname_lname) + "@" + str(domain)
+        self.email_id = str(fname_lname) + str(domain)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
+from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException, TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
@@ -47,45 +47,53 @@ while next_page_element and n < nos:
         article_link_list.append(article_link)
 
     try:
-        next_page_element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH,
+        next_page_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,
                                                                                             '//*[@id="lite-page"]/main/section[3]/div[4]/div/div/div[100]/div/div/div/div[1]/div/div[12]/a')))
         next_page_element.click()
     except ElementClickInterceptedException:
         footer_element = driver.find_element(By.XPATH, '//*[@id="qc-cmp2-ui"]/div[2]/div/button[3]')
         footer_element.click()
-        next_page_element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH,
+        next_page_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,
                                                                                             '//*[@id="lite-page"]/main/section[3]/div[4]/div/div/div[100]/div/div/div/div[1]/div/div[12]/a')))
         next_page_element.click()
 
     n += len(article_link_list)
 
 # Browse through each article link and get the required details. Store them in csv format.
-for link in article_link_list:
+for link in article_link_list[:nos]:
     driver.get(link)
     try:
         article_name_element = driver.find_element(By.XPATH, '//*[@id="lite-page"]/main/section/section[1]/div/div/h1')
         article_name = article_name_element.text
-    except NoSuchElementException:
+    except NoSuchElementException or TimeoutException:
         article_name = "NotFound"
+
     try:
         author_wi_elements = driver.find_elements(By.XPATH,
                                               '//*[@id="lite-page"]/main/section/section[1]/div/div/div[3]/div/div/div/div/div[2]/div/div/div/div/div/a')
         article_author_list = [author.get_property("text") for author in author_wi_elements]
-    except NoSuchElementException:
+        author_profile_list = [author.get_property("href") for author in author_wi_elements]
+    except NoSuchElementException or TimeoutException:
         article_author_list = []
+        author_profile_list = []
+
     try:
         inst_elements = driver.find_elements(By.XPATH,
-                                         '//*[@id="lite-page"]/main/section/section[1]/div/div/div[3]/div/div/div/div/div[2]/div/div/div/div/ul/li/span/a')
+                                             '//*[@id="lite-page"]/main/section/section[1]/div/div/div[3]/div/div/div/div/div[2]/div/div/div/div/ul/li/span/a')
         inst_list = [inst.get_property("text") for inst in inst_elements]
-    except NoSuchElementException:
+        inst_link_list = [inst.get_property("href") for inst in inst_elements]
+    except NoSuchElementException or TimeoutException:
         inst_list = []
-    author_inst_list = list(zip(article_author_list, inst_list))
+        inst_link_list = []
+
+    author_inst_list = list(zip(article_author_list, author_profile_list, inst_list, inst_link_list))
+
     try:
         author_wo_elements = driver.find_elements(By.XPATH,
                                               '//*[@id="lite-page"]/main/section/section[1]/div/div/div[3]/div/div/div[1]/div/div/div[2]/div/div/div/div/div/a')
         for a in range(len(author_wo_elements)):
-            author_inst_list.append((author_wo_elements[a].get_property("text"), " "))
-    except NoSuchElementException:
+            author_inst_list.append((author_wo_elements[a].get_property("text"), author_wo_elements[a].get_property("href"), " ", " "))
+    except NoSuchElementException or TimeoutException:
         None
 
     new_row = pd.Series({'Title': article_name,
@@ -93,6 +101,38 @@ for link in article_link_list:
                          })
     df = pd.concat([df, new_row.to_frame().T], ignore_index=False)
     df.to_csv(f'{topic}.csv', index=False)
-    time.sleep(10)
+    time.sleep(5)
 
 df.to_csv(f'{topic}.csv', index=False)
+
+# Get email id and prepare final excel
+df = pd.read_csv(f"{topic}.csv")
+df['Author_Institution'] = df['Author_Institution'].apply(lambda x: ast.literal_eval(x))
+
+clean_df = pd.DataFrame(columns=["Title", "Author", "Author_Profile", "Institute", "Institute_Link", "Website", "Email"])
+
+for n in range(len(df)):
+    article_title = df["Title"][n]
+    for a in range(len(df["Author_Institution"][n])):
+        author = df["Author_Institution"][n][a][0]
+        author_profile = df["Author_Institution"][n][a][1]
+        institute = df["Author_Institution"][n][a][2]
+        institute_link = df["Author_Institution"][n][a][3]
+        email_finder = EmailFinder()
+        email_finder.get_email(author, institute_link)
+        email = email_finder.email_id
+        website = email_finder.website
+
+        new_row = pd.Series({'Title': article_title,
+                             'Author': author,
+                             'Author_Profile': author_profile,
+                             'Institute': institute,
+                             'Institute_Link': institute_link,
+                             'Website': website,
+                             'Email': email
+                             })
+        clean_df = pd.concat([clean_df, new_row.to_frame().T], ignore_index=False)
+        time.sleep(5)
+
+with pd.ExcelWriter(f'{topic}.xlsx') as article_info:
+    clean_df.to_excel(article_info, index=False)
